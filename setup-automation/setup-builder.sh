e21f0241d445c7915fe341bc0b45d0397baf9329
Matt Micene (RHT) <mmicene@redhat.com>
2025-12-15 16:42:11 -0500

    Combine all the sections, update the UI with right modules

--- setup-automation/setup-builder.sh
+++ setup-automation/setup-builder.sh
@@ -92,35 +92,6 @@ ssh core@${VM_NAME}
 EOF
 
 chmod u+x /root/.wait_for_bootc_vm.sh
-#
-# Script that manages the ISO SSH session tab
-# Waits for the domain to start and networking before attempting to SSH to guest
-cat <<'EOF'> /root/.wait_for_iso_vm.sh
-echo "Waiting for VM 'iso-vm' to be running..."
-VM_READY=false
-VM_STATE=""
-VM_NAME=iso-vm
-while true; do
-    VM_STATE=$(virsh domstate "$VM_NAME" 2>/dev/null)
-    if [[ "$VM_STATE" == "running" ]]; then
-        VM_READY=true
-        break
-    fi
-    sleep 10
-done
-echo "Waiting for SSH to be available..."
-NODE_READY=false
-while true; do
-    if ping -c 1 -W 1 ${VM_NAME} &>/dev/null; then
-        NODE_READY=true
-        break
-    fi
-    sleep 5
-done
-ssh core@${VM_NAME}
-EOF
-
-chmod u+x /root/.wait_for_iso_vm.sh
 
 # Script that manages the VM SSH session tab
 # Waits for the domain to start and networking before attempting to SSH to guest
